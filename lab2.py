616b741376854d515098584a5eaec545427283eb
lizmontesdeoca5149 <liz.montesdeoca5149@gmail.com>
2025-06-17 16:59:04 -0700

    completed lab 2

---
+++ lab2.py
@@ -0,0 +1,22 @@
+#Lab 2: Python Profiles
+
+## Step 1
+name = "Lizbeth"
+"balloon"
+## Step 2
+nickname = "Liz"
+
+## Step 3
+age = 16
+
+## int data type examples: -2, 1000000 anything without a decimal
+## float data type examples: -2.0, 24.12312312313123123213123, anything with a decimal
+
+## Step 4
+used_python = False
+
+## Step 5
+hobbies = ["reading", "drawing", "hanging out with friends", "volleyball"]
+
+## Step 6
+favorite_things = {"book": "Omniscient Reader's Viewpoint", "color":"red and blue", 'food':'tamales'}
