91b382f05b0d1c2ae823cf2a024f74e46b562546
lizmontesdeoca5149 <liz.montesdeoca5149@gmail.com>
2025-07-22 17:25:07 -0700

    Completed guessing game project

---
+++ guessing_game.py
@@ -0,0 +1,44 @@
+# Guessing Game - Liz, Alvert, Anthony, Tammy
+import random
+
+def generate_random_number():
+    random_number = random.randint(1,100)
+    return random_number
+
+def get_user_guess():
+    while True:
+        user_input = input("Please enter a number between 1-100: ")
+        try:
+            if 1 <= int(user_input) <= 100:
+                return user_input
+        except:
+            print("This is not a valid number!")
+
+def play_guessing_game():
+    secret_number = generate_random_number()
+    Attempts = 0
+    while True:
+        guess = int(get_user_guess())
+        if guess > secret_number:
+            print('Your guess is too high!')
+            Attempts += 1
+            print(f'Attempts {Attempts}')
+        elif guess < secret_number:
+            print('Your guess is too low!')
+            Attempts += 1
+            print(f'Attempts {Attempts}')
+        else:
+            print(f'You win! You guessed the number in {Attempts} attempts!')
+            break
+        
+def game_loop():
+    while True:
+        play_guessing_game()
+
+        play_again = input('Would you like to play again? yes/no: ').lower()
+        if play_again == 'no':
+            print('Bye bye user')
+            break
+
+if __name__ == "__main__": 
+    game_loop()
