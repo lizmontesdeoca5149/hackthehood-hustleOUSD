57b81cfd8351da5baeeb4391f53a6bbf70f405e8
lizmontesdeoca5149 <liz.montesdeoca5149@gmail.com>
2025-07-29 16:38:25 -0700

    Updated + Completed lab4.py

--- lab4.py
+++ lab4.py
@@ -6,18 +6,38 @@ while checking == "yes":
     user_input = input()
 
     if int(user_input) >= 18:
-        print("Yes you can vote")
+        print("Yes, you can vote.")
 
     else:
-        print("No you can't vote")
-        print ("Would you like to check another age?")
+        print("No, you can't vote brochacho.")
+
+    def checking_loop():
+        while True:
+            if 1<= (user_input) <= 18:
+                return user_input
+              
+
+    user_input = input ("Would you like to check another age?(yes/no)").strip().lower()
+
+    if user_input == "yes":
+        continue
+
+    elif user_input == "no":
+        print("Bye bye. Returning to menu..")
+        break
+
+    else:
+        print ("Invalid Input. Please type in yes/no.")
+
+
+
     #user_input2 = input()
     #checking = user_input2
 
 #Task 2
-List = [-9,7,0]
-for i in list:
-    if i <0:
+Number_List = [-9,7,0,10,20,37,52]
+for i in Number_List:
+    if i > 0:
         print('positive')
     elif i < 0:
         print('negative')
@@ -30,8 +50,8 @@ inventory = ("tnt", "grass", "netherite", "waxed lightly weathered chiseled copp
 for i in inventory:
     if i == "waxed lightly weathered chiseled copper stairs":
         print("why do you have this in your inventory?")
-    elif i == "tnt":
-        print("tnt")
 
+    print (i)
+    
 
 
